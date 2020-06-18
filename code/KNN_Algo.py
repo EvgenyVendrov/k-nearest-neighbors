8f61703c4fcaed67efdac00f57d3d5461676051f
Evgeny Vendrov <evgenyv94@gmail.com>
2020-06-18 14:58:01 +0300

    added tests for train set too

--- code/KNN_Algo.py
+++ code/KNN_Algo.py
@@ -4,20 +4,29 @@ import calculations
 
 def knn(num_of_rounds=500, path_for_data_set=r"dataset\HC_Body_Temperature.txt"):
     (data_set) = data_prep.get_data_set_from_path(path_for_data_set)
-    dict_of_accuracy_by_k_and_p = {}
+    dict_of_test_accuracy_by_k_and_p = {}
+    dict_of_train_accuracy_by_k_and_p = {}
     for i in range(0, num_of_rounds):
         (shuffled_train_set, shuffled_test_set) = data_prep.shuffle_dataset(data_set)
         for k in range(1, 10, 2):
             for p in range(1, 4):
-                dict_of_accuracy_by_k_and_p["%dk_%dp_%d" % (k, p, i)] = calculations.get_acc_on_train_set(
+                dict_of_test_accuracy_by_k_and_p["%dk_%dp_%d" % (k, p, i)] = calculations.get_acc_on_train_set(
                     shuffled_train_set,
                     shuffled_test_set,
                     k, p)
-    list_of_acc_avg = calculations.get_avg_error_by_k_and_p(dict_of_accuracy_by_k_and_p)
+                dict_of_train_accuracy_by_k_and_p["%dk_%dp_%d" % (k, p, i)] = calculations.get_acc_on_train_set(
+                    shuffled_train_set,
+                    shuffled_train_set,
+                    k, p)
+    list_of_test_acc_avg = calculations.get_avg_error_by_k_and_p(dict_of_test_accuracy_by_k_and_p)
+    list_of_train_acc_avg = calculations.get_avg_error_by_k_and_p(dict_of_train_accuracy_by_k_and_p)
     i = 0
     for k in range(1, 10, 2):
         for p in range(1, 4):
-            print_avg(k, p, list_of_acc_avg[i])
+            print("TRAIN:")
+            print_avg(k, p, list_of_train_acc_avg[i])
+            print("TEST:")
+            print_avg(k, p, list_of_test_acc_avg[i])
             i += 1
 
 
